3100ce6a4631e5e9db15523e15ad10ea30866258
NIKITA <Nikita.bort87@yandex.ru>
2023-04-01 16:50:37 +0300

    AФdded a task solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[] arr1 = new string[5] {"345", "18", "welcome", "music", "map"};
+string[] arr2 = new string[arr1.Length];
+void FinishArr(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+FinishArr(arr1, arr2);
+PrintArr(arr2);
